8664b524889355e497b6cafac3eb4ae10afde855
Park Ian Co <opensource@parkian.co>
2026-03-11 21:24:28 -0400

    Add placeholder test file

---
+++ test/test-multisig.lisp
@@ -0,0 +1,7 @@
+;;;; test/test-multisig.lisp - Tests for CL-MULTISIG-PROTOCOLS
+
+(in-package #:cl-multisig-protocols.test)
+
+(defun run-all-tests ()
+  (format t "~&No tests defined yet.~%")
+  t)
